fb01a8b5a8401647b5b9a4cad044635f26f97d32
”wwowann” <ivasin75@mail.ru>
2021-02-07 23:50:34 +0000

    add all

--- src/main/java/CarsSalon.java
+++ src/main/java/CarsSalon.java
@@ -4,49 +4,40 @@ import java.util.List;
 public class CarsSalon {
     List<Car> cars = new ArrayList<>(10);
 
-//     вариант кода ниже тоже работает, но только покупатели все вместе
-// приходят в магазин, а потом в том же порядке уходят с покупкой
+    public void sellCar() {
 
-    public Car sellCar() {
         System.out.println(Thread.currentThread().getName() + " покупает");
-
         try {
-            synchronized (this){if (cars.size() == 0) {
+            synchronized (this) {
+                if (cars.size() == 0) {
+                    System.out.println("Автомобилей нет на складе, подождите привоза новых автомобилей");
                     wait();
                 }
             }
-            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
-        return cars.remove(0);
+        if (Thread.currentThread().isAlive() && cars.size() > 0) {
+            cars.remove(0);
+        }
     }
 
     public void acceptCar() {
-        cars.add(new Car());
-            synchronized (this){notify();}
+        int count = 0;
+        try {
+            while (count < 11) {
+                System.out.println("Производитель выпустил новый автомобиль. Можно продавать");
+                synchronized (this) {
+                    cars.add(new Car());
+                    count++;
+                    notifyAll();
+                    Thread.sleep(3000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Производство автомобилей закончено, кина не будет");
     }
-
-//    public synchronized Car sellCar() {
-//        System.out.println(Thread.currentThread().getName() + " покупает");
-//
-//        try {
-//            if (cars.size() == 0) {
-//                System.out.println("Автомобилей нет на складе, подождите привоза новых автомобилей");
-//                wait();
-//            }
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
-//        return cars.remove(0);
-//    }
-//
-//    public synchronized void acceptCar() {
-//        System.out.println("Производитель выпустил новый автомобиль. Можно продавать");
-//        cars.add(new Car());
-//        notify();
-//    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         CarsSalon carsSalon = new CarsSalon();
-        int counter = 0;
-        System.out.println("Всего машин в салоне " + carsSalon.cars.size());
+        Thread threadProducedCar = new Thread();
+        new Thread(null, carsSalon::acceptCar, "Продавец ").start();
+        startThread(carsSalon);
+    }
 
-        while (counter < 11) {
+    public static void startThread(CarsSalon carsSalon) throws InterruptedException {
+        for (int i = 0; i < 4; i++) {
             new Thread(null, carsSalon::sellCar, "Покупатель 1 ").start();
             new Thread(null, carsSalon::sellCar, "Покупатель 2 ").start();
             new Thread(null, carsSalon::sellCar, "Покупатель 3 ").start();
-
-            for (int i = 0; i < 3; i++) {
-                new Thread(null, carsSalon::acceptCar, "Продавец ").start();
-                counter++;
-                Thread.sleep(2000);
-            }
+            Thread.sleep(5000);
+            System.out.println("");
         }
-      }
+    }
 }
 
 
